458790c884db00dc474c3f1d0cafb97f3bbdc506
Dannie Vinther <davi@kea.dk>
2024-05-11 20:00:43 +0200

    chore: Refactor prefix function in utils.js for better readability

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,9 +5,14 @@
 // const classReg = /(?<!\{[^}]*)[^,/*&{}\s]+(?![^{]*\})/g;
 const classReg = /(?<!\{(?:[^\{\}]|\{[^\{\}]*\})*?)[^,/*&{}\s]+(?!.*\})/g;
 
+// export function prefix(str, exerciseKey) {
+//   return str.replaceAll(
+//     classReg,
+//     (match) => `[data-exercise-key="${exerciseKey}"] .output ${match}`
+//   );
+// }
 export function prefix(str, exerciseKey) {
-  return str.replaceAll(
-    classReg,
-    (match) => `[data-exercise-key="${exerciseKey}"] .output ${match}`
-  );
+  return `[data-exercise-key="${exerciseKey}"] .output {
+    ${str}
+}`;
 }
