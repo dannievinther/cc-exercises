4bba88eb78138c4b8d95d0a6704bdeac363f01fe
Dannie Vinther <davi@kea.dk>
2024-12-06 23:23:05 +0100

    enable responsive images and client prerendering in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,15 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    experimentalLayout: "responsive",
+  },
+  experimental: {
+    responsiveImages: true,
+    clientPrerender: true,
+  },
+  prefetch: {
+    prefetchAll: true,
+  },
   integrations: [icon()],
 });
