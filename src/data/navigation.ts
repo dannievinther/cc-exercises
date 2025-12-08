26017509fd965cddf08b20d6cc6d3d05daa58a32
Dannie Vinther <davi@kea.dk>
2025-12-08 14:31:55 +0100

    feat: implement navigation structure with dropdown for exercises and enhance header components

---
+++ src/data/navigation.ts
@@ -0,0 +1,40 @@
+import GridIcon from "../icons/grid2.svg";
+import SubgridIcon from "../icons/subgrid2.svg";
+import FlexboxIcon from "../icons/flex.svg";
+import SdaIcon from "../icons/sda2.svg";
+
+export interface NavItem {
+  href: string;
+  text: string;
+  longText?: string;
+  icon: typeof GridIcon;
+  collection: "grid" | "subgrid" | "flexbox" | "sda";
+  separator?: boolean;
+}
+
+export const navItems: NavItem[] = [
+  { href: "/grid", text: "Grid", icon: GridIcon, collection: "grid" },
+  {
+    href: "/subgrid",
+    text: "Subgrid",
+    icon: SubgridIcon,
+    collection: "subgrid",
+  },
+  { href: "/flex", text: "Flexbox", icon: FlexboxIcon, collection: "flexbox" },
+  {
+    href: "/sda",
+    text: "Scroll",
+    longText: "Scroll Animations",
+    icon: SdaIcon,
+    collection: "sda",
+    separator: true,
+  },
+];
+
+export type Exercise = {
+  id: string;
+  data: { title: string; draft?: boolean };
+};
+
+export const isCurrentPath = (pathname: string, href: string): boolean =>
+  pathname.replace(/\/$/, "").startsWith(href);
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// Extend HTML anchor attributes for experimental web platform features
+declare namespace astroHTML.JSX {
+  interface AnchorHTMLAttributes {
+    interestfor?: string;
+  }
+}
